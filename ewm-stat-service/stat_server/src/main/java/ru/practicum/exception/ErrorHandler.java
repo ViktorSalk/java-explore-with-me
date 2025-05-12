ddb0350a59d112ee75491c6d94500c09cd26414f
Viktor64 <ecogorod64@mail.ru>
2025-05-12 22:18:26 +0400

    1) Создал ветку stat_svc. 2) Подготовил многомодульность: Основной сервис ewm-main-service и Сервис статистики ewm-stat-service. 3) Разделил Сервис на два подмодуля: клиентскую часть и серверную часть: stat_client и stat_server. 4) Выделил третий подмодуль с DTO-объектами для передачи данных. 5) Проект настроен для работы с Docker через файл docker-compose.yml. 6) Добавил postman, обновил pom и gitignore.

--- ewm-stat-service/stat_server/src/main/java/ru/practicum/exception/ErrorHandler.java
+++ ewm-stat-service/stat_server/src/main/java/ru/practicum/exception/ErrorHandler.java
@@ -1,6 +1,5 @@
 package ru.practicum.exception;
 
-import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +9,7 @@ import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import jakarta.validation.ConstraintViolationException;
 import java.time.LocalDateTime;
 
 import static ru.practicum.utill.Constants.FORMATTER;
